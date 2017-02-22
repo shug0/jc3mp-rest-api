80c4a6fb259b227c615918f4a4bb9e92cd502254
Thomas Alberola <thomas@needacoffee.fr>
2017-02-22 23:30:29 +0100

    Adding CORS module for cross origin problem

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // Node modules
 const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
 const logger = require('morgan');
 const http = require('http');
+const cors = require('cors');
 
 // Custom modules
 const config = require('./config');
@@ -13,24 +13,25 @@ const loadRoutes = require('./api/index');
 const port = config.port;
 const app = express();
 
+// Allow Crossorigin
+app.use(cors());
+
+// load routes
 loadRoutes(app);
 
-// Dunno why using it
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   res.json({
     error: err
   })
 });
+
 
 var server = http.createServer(app);
 server.listen(port);
